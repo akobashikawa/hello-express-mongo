0ea4cbd7300710cffbb883259571c7770172bb58
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-01-25 13:14:31 -0500

    Add visual indicators
    Simplify loading indicator logic cause axios

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,26 @@
+Vue.use(Toasted, {
+    position: 'bottom-center',
+    duration: 2000
+});
+
+axios.interceptors.request.use(function (config) {
+    app.loading = true;
+    return config;
+}, function (error) {
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function (response) {
+    app.loading = false;
+    return response;
+}, function (error) {
+    return Promise.reject(error);
+});
+
 const app = new Vue({
     el: '#app',
     data: {
-        urlBase: window.location.origin + '/api/tasks',
+        baseURL: window.location.origin + '/api/tasks',
         tasks: [],
         newTask: {
             description: '',
@@ -9,34 +28,39 @@ const app = new Vue({
         },
         loading: false,
     },
+    components: {
+    },
     created: function () {
         this.getTasks();
     },
     mounted: function () {
-        // console.log(this.$refs);
     },
     methods: {
         getTasksService: async function () {
-            const url = `${this.urlBase}/`;
+            const url = `${this.baseURL}/`;
             try {
                 const result = await axios.get(url);
-                return result.data;
+                if (typeof result.data === 'object') {
+                    return result.data;
+                }
+                return '';
             } catch (error) {
                 console.log(error);
             }
         },
         getTasks: async function () {
-            this.loading = true;
             try {
                 this.tasks = await this.getTasksService();
-                this.loading = false;
             } catch (error) {
                 console.log(error);
+                this.$toasted.show("Problem to list the tasks", {
+                    type: 'error'
+                });
             }
         },
 
         addTaskService: async function (data) {
-            const url = `${this.urlBase}/`;
+            const url = `${this.baseURL}/`;
             try {
                 const result = await axios.post(url, data);
                 return result.data;
@@ -45,17 +69,19 @@ const app = new Vue({
             }
         },
         addTask: async function () {
-            this.loading = true;
             try {
                 await this.addTaskService(this.newTask);
                 this.getTasks();
             } catch (error) {
                 console.log(error);
+                this.$toasted.show("Problem to add the task", {
+                    type: 'error'
+                });
             }
         },
 
         deleteTaskService: async function (id) {
-            const url = `${this.urlBase}/${id}`;
+            const url = `${this.baseURL}/${id}`;
             try {
                 const result = await axios.delete(url);
                 return result.data;
@@ -64,12 +90,14 @@ const app = new Vue({
             }
         },
         deleteTask: async function (task) {
-            this.loading = true;
             try {
                 await this.deleteTaskService(task._id);
                 this.getTasks();
             } catch (error) {
                 console.log(error);
+                this.$toasted.show("Problem to delete the task", {
+                    type: 'error'
+                });
             }
         },
 
@@ -86,7 +114,7 @@ const app = new Vue({
         },
 
         updateTaskService: async function (task) {
-            const url = `${this.urlBase}/${task._id}`;
+            const url = `${this.baseURL}/${task._id}`;
             try {
                 const result = await axios.put(url, task);
                 return result;
@@ -95,7 +123,6 @@ const app = new Vue({
             }
         },
         saveTask: async function (task) {
-            this.loading = true;
             this.$set(task, 'editing', false);
             this.$set(task, 'description', task.descriptionUpdate);
             try {
@@ -103,11 +130,13 @@ const app = new Vue({
                 this.getTasks();
             } catch (error) {
                 console.log(error);
+                this.$toasted.show("Problem to save the task", {
+                    type: 'error'
+                });
             }
         },
 
         doneTask: async function (task) {
-            this.loading = true;
             this.$set(task, 'editing', false);
             this.$set(task, 'done', true);
             try {
@@ -115,10 +144,12 @@ const app = new Vue({
                 this.getTasks();
             } catch (error) {
                 console.log(error);
+                this.$toasted.show("Problem to update the task status", {
+                    type: 'error'
+                });
             }
         },
         undoneTask: async function (task) {
-            this.loading = true;
             this.$set(task, 'editing', false);
             this.$set(task, 'done', false);
             try {
@@ -126,6 +157,9 @@ const app = new Vue({
                 this.getTasks();
             } catch (error) {
                 console.log(error);
+                this.$toasted.show("Problem to update the task status", {
+                    type: 'error'
+                });
             }
         },
     }
