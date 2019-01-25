4604707a76d63ebe83dfb13e8184fdc369ed505c
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-01-25 17:21:23 -0500

    Enhance schema def

--- models/index.js
+++ models/index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 mongoose.connect(`${process.env.MONGO_URL}`, { useNewUrlParser: true });
 
-exports.Task = mongoose.model('Task', { description: String, done: Boolean });
-exports.User = mongoose.model('User', { username: String, password: String });
+exports.Task = mongoose.model('Task', {
+    description: { type: String, required: true, trim: true },
+    done: Boolean
+});
+
+exports.User = mongoose.model('User', {
+    username: { type: String, required: true, trim: true },
+    password: { type: String, required: true }
+});
