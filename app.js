32f2c2b11fe7fe9c250f2be60412482fc6329589
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-04-01 17:57:25 -0500

    Adding fallback

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
+const history = require('connect-history-api-fallback');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -31,9 +32,15 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/api/tasks', tasksRouter);
 app.use('/api/users', usersRouter);
+
+// public
+app.use('/', express.static('public'));
+app.use(history({
+  verbose: true
+}));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
