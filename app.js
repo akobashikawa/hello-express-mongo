f545f3bab68f654e90f528bc80cc6db26af6bf68
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-04-01 17:59:58 -0500

    Fix fallback order

--- app.js
+++ app.js
@@ -37,10 +37,10 @@ app.use('/api/tasks', tasksRouter);
 app.use('/api/users', usersRouter);
 
 // public
-app.use('/', express.static('public'));
 app.use(history({
   verbose: true
 }));
+app.use('/', express.static('public'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
